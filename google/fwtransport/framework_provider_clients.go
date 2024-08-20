df964bc7f4a058b1a4a2365a6ce9be8e7cbe4c9b
Sarah French <sarah.french@hashicorp.com>
2024-08-20 19:53:29 +0100

    Delete unneeded files

--- google/fwtransport/framework_provider_clients.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-package fwtransport
-
-// Remove fwtransport.(p *FrameworkProviderConfig).NewDnsClient
-// Instead use transport.(c *Config).NewDnsClient
--- google/fwtransport/framework_transport.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-package fwtransport
-
-// Remove fwtransport.SendFrameworkRequest
-// Instead use transport_tpg.SendRequest
-
-// Remove fwtransport.SendFrameworkRequestWithTimeout
-// Instead use transport_tpg.(b *RequestBatcher).SendRequestWithTimeout
